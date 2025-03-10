1b8317523f5ed94288da51bb97474852e9f05da8
Mateusz Holowieszko <holowieszko.m@gmail.com>
2025-03-10 12:34:49 +0100

    feat(external): added grafana secret

--- external/secretsmanager.tf
+++ external/secretsmanager.tf
@@ -1,7 +1,7 @@
 module "speedtest_db_secrets" {
   source      = "./modules/secrets-manager/v1"
   secret_name = replace(format("%s-databases-speedtest-credentials", module.default_label.id), "-", "/")
-  description = "Homelab Speedtest PostgreSQL database credentials (push)"
+  description = "Homelab Speedtest PostgreSQL database credentials"
 }
 
 module "speedtest_app_secrets" {
@@ -20,6 +20,12 @@ module "argocd_app_secrets" {
   source      = "./modules/secrets-manager/v1"
   secret_name = replace(format("%s-applications-argocd-credentials", module.default_label.id), "-", "/")
   description = "Homelab ArgoCD credentials (push)"
+}
+
+module "grafana_app_secrets" {
+  source      = "./modules/secrets-manager/v1"
+  secret_name = replace(format("%s-applications-grafana-credentials", module.default_label.id), "-", "/")
+  description = "Homelab Grafana credentials"
 }
 
 module "global_config_secrets" {
